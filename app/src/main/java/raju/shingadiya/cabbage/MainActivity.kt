7a38ca35c25f2253f71b4e44683881760c517ecb
Raju Shingadiya <raju348636@gmail.com>
2022-12-22 14:52:18 +0530

    changes

--- app/src/main/java/raju/shingadiya/cabbage/MainActivity.kt
+++ app/src/main/java/raju/shingadiya/cabbage/MainActivity.kt
@@ -1,22 +1,12 @@
 package raju.shingadiya.cabbage
 
+import android.content.ActivityNotFoundException
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
-
-
-
-@Cabbage
-class Collage(context: Context,size:Int){
-    var name:String?=null
-
-    fun addFullName(fullname:String){
-
-    }
-}
-
-
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,17 +14,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val home=Collage(this,10)
-        home.name="abnrios"
-        home.addFullName("andirud")
 
-        val home1= buildCollage(this,10) {
-            name="raju"
-            addFullName("Andorid")
-        }
+       /* val linkaddresss = "https://www.instagram.com/reel/Cip21PqjUen/?igshid=ZTA1ZTQyMGU="
 
+        openInstaLink(linkaddresss)
+*/
+
+        /*  Intent(Intent.ACTION_VIEW, Uri.parse(link)).apply {
+                   setPackage("com.instagram.android")
+               }.let {intent->
+                   runCatching {
+
+                   }
+               }*/
 
     }
+
+  /*  fun Context.openInstaLink(link: String) {
+        val likeIng = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+        likeIng.setPackage("com.instagram.android")
+        try {
+            startActivity(likeIng)
+        } catch (e: ActivityNotFoundException) {
+            Log.e("AndroidError:", e.message.toString())
+        }
+    }*/
 
 
 }
